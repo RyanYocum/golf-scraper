1e92cd7564329e05efd5c299fb7740a86c8e4cc2
Ryan <RyanCYcoum@gmail.com>
2015-12-08 22:55:54 +0000

    database entry now puts entries into database in series

--- jsonDatabaseEntry.js
+++ jsonDatabaseEntry.js
@@ -23,7 +23,22 @@ var Course = sequelize.define('course', {
 
 sequelize.sync()
 
-for (var i = 0; i < data.length; i++) {
+//puts all of the entries into the database one by one, using a for looped seemed to have caused many to be left out
+
+var databaseEntry = function (i) {
+	console.log(i);
 	var j = data[i];
-	Course.create({name: j.name, city: j.city, county: j.county, par: j.par, hdcp: j.hdcp, parL: j.parL, hdcpL: j.hdcpL})
+	Course.create({name: j.name, city: j.city, county: j.county, par: j.par, hdcp: j.hdcp, parL: j.parL, hdcpL: j.hdcpL}).done(function (course) {
+		i++;
+
+		//manually removed error, should be automated
+		if (i == 97) {
+			i++
+		}
+
+		if (i < data.length - 1) {
+			databaseEntry(i)
+		}
+	})
 }
+databaseEntry(0);
